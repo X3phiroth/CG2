b5820f58e075ce2d32930c393eac308c0b1f996b
X3phiroth <ch.kozielski@icloud.com>
2015-11-25 23:40:15 +0100

    Started Aufgabe 2.2
    prepared folder

---
+++ Aufgabe 2/cg-a02.2-meshes/controller/html_controller.js
@@ -0,0 +1,170 @@
+/*
+ * JavaScript / Canvas teaching framwork 
+ * (C)opyright Kristian Hildebrand, khildebrand@beuth-hochschule.de
+ *
+ * Module: html_controller
+ *
+ * Defines callback functions for communicating with various 
+ * HTML elements on the page, e.g. buttons and parameter fields.
+ *
+ */
+
+
+/* requireJS module definition */
+define(["jquery", "BufferGeometry", "random", "band", "parametric"],
+        (function ($, BufferGeometry, Random, Band, ParametricSurface) {
+            "use strict";
+            /*
+             * define callback functions to react to changes in the HTML page
+             * and provide them with a closure defining context and scene
+             */
+            var HtmlController = function (scene) {
+
+
+                $("#random").show();
+                $("#band").hide();
+                $("#ellipsoid").hide();
+                $("#parametric").hide();
+
+                $("#btnRandom").click((function () {
+                    $("#random").show();
+                    $("#band").hide();
+                    $("#ellipsoid").hide();
+                    $("#parametric").hide();
+                }));
+                $("#btnBand").click((function () {
+                    $("#random").hide();
+                    $("#band").show();
+                    $("#ellipsoid").hide();
+                    $("#parametric").hide();
+                }));
+                $("#btnEllipsoid").click((function () {
+                    $("#random").hide();
+                    $("#band").hide();
+                    $("#ellipsoid").show();
+                    $("#parametric").hide();
+                }));
+                $("#btnParametric").click((function () {
+                    $("#random").hide();
+                    $("#band").hide();
+                    $("#ellipsoid").hide();
+                    $("#parametric").show();
+                    $("#btnNewParametric").show();
+                    $("#btnNewPillow").hide();
+                    $("#btnNewCosine").hide();
+
+                }));
+                $("#btnPillow").click((function () {
+                    $("#random").hide();
+                    $("#band").hide();
+                    $("#ellipsoid").hide();
+                    $("#parametric").show();
+                    $("#btnNewParametric").hide();
+                    $("#btnNewPillow").show();
+                    $("#btnNewCosine").hide();
+                }));
+                $("#btnCosin").click((function () {
+                    $("#random").hide();
+                    $("#band").hide();
+                    $("#ellipsoid").hide();
+                    $("#parametric").show();
+                    $("#btnNewParametric").hide();
+                    $("#btnNewPillow").hide();
+                    $("#btnNewCosine").show();
+                }));
+                $("#btnNewRandom").click((function () {
+
+                    var numPoints = parseInt($("#numItems").attr("value"));
+                    var random = new Random(numPoints);
+                    var bufferGeometryRandom = new BufferGeometry();
+                    bufferGeometryRandom.addAttribute("position", random.getPositions());
+                    bufferGeometryRandom.addAttribute("color", random.getColors());
+                    scene.addBufferGeometry(bufferGeometryRandom);
+                }));
+                $("#btnNewBand").click((function () {
+
+                    var config = {
+                        segments: parseInt($("#numSegments").attr("value")),
+                        radius: parseInt($("#radius").attr("value")),
+                        height: parseInt($("#height").attr("value"))
+                    };
+                    var band = new Band(config);
+                    var bufferGeometryBand = new BufferGeometry();
+                    bufferGeometryBand.addAttribute("position", band.getPositions());
+                    bufferGeometryBand.addAttribute("color", band.getColors());
+                    scene.addBufferGeometry(bufferGeometryBand);
+                }));
+                $("#btnNewParametric").click((function () {
+
+                    var config = {
+                        vsegments: parseInt($("#parVSegments").attr("value")),
+                        usegments: parseInt($("#parUSegments").attr("value")),
+                        umin: parseFloat($("#parUMin").attr("value")),
+                        umax: parseFloat($("#parUMax").attr("value")),
+                        vmin: parseFloat($("#parVMin").attr("value")),
+                        vmax: parseFloat($("#parVMax").attr("value"))
+
+                    };
+                    var positionFunc = function (u, v) {
+                        return [400 * Math.sin(u) * Math.cos(v),
+                            200 * Math.sin(u) * Math.sin(v),
+                            100 * Math.cos(u)];
+                    };
+                    var parametric = new ParametricSurface(positionFunc, config);
+                    var bufferGeometryParametric = new BufferGeometry();
+                    bufferGeometryParametric.addAttribute("position", parametric.getPositions());
+                    bufferGeometryParametric.addAttribute("color", parametric.getColors());
+                    scene.addBufferGeometry(bufferGeometryParametric);
+                }));
+                $("#btnNewPillow").click((function () {
+
+                    var config = {
+                        vsegments: parseInt($("#parVSegments").attr("value")),
+                        usegments: parseInt($("#parUSegments").attr("value")),
+                        umin: parseFloat($("#parUMin").attr("value")),
+                        umax: parseFloat($("#parUMax").attr("value")),
+                        vmin: parseFloat($("#parVMin").attr("value")),
+                        vmax: parseFloat($("#parVMax").attr("value"))
+
+                    };
+                    var positionFunc = function (u, v) {
+                        return [300 * Math.cos(u),
+                            300 * Math.cos(v),
+                            150 * Math.sin(u) * Math.sin(v)];
+                    };
+                    var parametric = new ParametricSurface(positionFunc, config);
+                    var bufferGeometryParametric = new BufferGeometry();
+                    bufferGeometryParametric.addAttribute("position", parametric.getPositions());
+                    bufferGeometryParametric.addAttribute("color", parametric.getColors());
+                    scene.addBufferGeometry(bufferGeometryParametric);
+                }));
+                $("#btnNewCosine").click((function () {
+
+                    var config = {
+                        vsegments: parseInt($("#parVSegments").attr("value")),
+                        usegments: parseInt($("#parUSegments").attr("value")),
+                        umin: parseFloat($("#parUMin").attr("value")),
+                        umax: parseFloat($("#parUMax").attr("value")),
+                        vmin: parseFloat($("#parVMin").attr("value")),
+                        vmax: parseFloat($("#parVMax").attr("value"))
+
+                    };
+                    var positionFunc = function (u, v) {
+                        return [400 * Math.cos(u),
+                            200 * Math.cos(v),
+                            200 * Math.cos(u + v)];
+                    };
+                    var parametric = new ParametricSurface(positionFunc, config);
+                    var bufferGeometryParametric = new BufferGeometry();
+                    bufferGeometryParametric.addAttribute("position", parametric.getPositions());
+                    bufferGeometryParametric.addAttribute("color", parametric.getColors());
+                    scene.addBufferGeometry(bufferGeometryParametric);
+                }));
+            };
+            // return the constructor function
+            return HtmlController;
+        })); // require
+
+
+
+
---
+++ Aufgabe 2/cg-a02.2-meshes/models/parametric.js
@@ -0,0 +1,69 @@
+/*
+ * JavaScript / Canvas teaching framwork
+ * (C)opyright Kristian Hildebrand, khildebrand@beuth-hochschule.de
+ *
+ * Module: ParametricSurface
+ *
+ */
+
+/* requireJS module definition */
+define(["three"],
+        (function (THREE) {
+
+            "use strict";
+
+            var ParametricSurface = function (posFunc, config) {
+
+                var umin = config.umin || -Math.PI;
+                var umax = config.umax || Math.PI;
+                var vmin = config.vmin || -Math.PI;
+                var vmax = config.vmax || Math.PI;
+                var usegments = config.usegments || 20;
+                var vsegments = config.vsegments || 10;
+
+                this.posFunc = posFunc;
+
+                this.positions = new Float32Array( (usegments + 1) * (vsegments + 1) * 3);
+                this.colors = new Float32Array( (usegments + 1) * (vsegments + 1) * 3);
+
+                var umin_temp = 0;
+                var vmin_temp = 0;
+
+//                var c = 0;
+                for (var i = 0; i <= usegments; i++) {
+                    umin_temp += (umax - umin) / usegments; // errechnet position u
+                    vmin_temp = vmin;
+                    for (var j = 0; j <= vsegments; j++) {
+                        vmin_temp += (vmax - vmin) / vsegments; // errechnet position v
+                        // errechnet Koordinaten, abhängig von Funktion
+                        this.positions[(i * vsegments + j) * 3] = this.posFunc(umin_temp, vmin_temp)[0]; // (i * vsegments + j) * 3
+ //                       c++;
+                        this.positions[(i * vsegments + j) * 3 + 1] = this.posFunc(umin_temp, vmin_temp)[1]; // (i * vsegments + j) * 3 + 1
+ //                       c++;
+                        this.positions[(i * vsegments + j) * 3 + 2] = this.posFunc(umin_temp, vmin_temp)[2]; // (i * vsegments + j) * 3 + 2
+ //                       c++;
+                    };
+                };
+                
+                this.color = new THREE.Color();
+                this.color.setRGB( 0,0,1 );
+                for (var k = 0; k < this.positions.length; k+=3) {
+                    this.colors[k]   = this.color.r;
+                    this.colors[k+1] = this.color.g;
+                    this.colors[k+2] = this.color.b;
+                }
+                console.log("Test");
+
+                this.getPositions = function () {
+                    return this.positions;
+                };
+
+                this.getColors = function () {
+                    return this.colors;
+                };
+
+            };
+
+            return ParametricSurface;
+        }));
+
